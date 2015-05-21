a63b139275e8dd5805edaf41f2a2127e4d6938b4
crsimal <ottisbbx@gmail.com>
2015-05-21 11:05:32 +0200

    Validate.js y comienzo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+$("miFormulario").validate({
+    rules: {
+    	dni:{
+    		nifES: true,
+    		required: true
+    	}
+    },
+    messages: {
+    	usuario:{
+    			remote: "El usuario ya existe en la bbdd."
+    	}
+    	email:{
+    			remote: "Ya existe este mail"
+    	}
+    },
+    submitHandler:
+}
+});
