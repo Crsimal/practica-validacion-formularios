c36249bf791a51517de2bd9f1431891a048305f3
Crsimal <cros@cros-IMEDIA-6550-AIO.(none)>
2015-06-08 13:39:05 +0200

    Formulario terminado

--- app/php/terminar.php
+++ app/php/terminar.php
@@ -2,28 +2,60 @@
 <html>
 <head>
 	<title>Enviar</title>
+	<link rel="stylesheet" href="../styles/main.css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+	<style type="text/css">
 
+
+        body {
+        		text-align:center;
+        		padding-top: 15%;
+        		font-weight: bolder;
+        		background-color: #DADADA;
+        		font-family: 'Roboto', sans-serif;
+        }
+
+        h2{
+        	text-decoration: underline;
+        }
+
+</style>
 </head>
 <body>
 
-
+<h2>Terminar Registro</h2>
 <?php
 $mensualidad=$_POST['pago'];
 
-if($mensualidad="Mensual"){
+if($mensualidad=="Mensual"){
 	$mensualidad="50";
 }
-if($mensualidad="Trimestral"){
+if($mensualidad=="Trimestral"){
 	$mensualidad="140";
 }
-if($mensualidad="Anual"){
+if($mensualidad=="Anual"){
 	$mensualidad="550";
 }
 
-echo "A continuacion sera registrado y procederemos a cobrarle la cantidad de $mensualidad euros";
+echo "A continuacion sera registrado y procederemos a cobrarle la cantidad de $mensualidad euros ¿Desea continuar?";
 
-echo "<br><a href='../index.html' >Aceptar</a>  <a href='../index.html'>Cancelar</a>";
+
 ?>
+<br/><br/>
+<form method="POST" id="formu">
+    <button type="submit" id="terminar" name="terminar">Aceptar</button>
+    <button type="submit" id="terminar2" name="terminar2">Cancelar</button>
 
+	</form>
+<?php 
+
+if(isset($_POST['terminar'])){
+	header('Location: http://localhost/dist/index.html');
+}
+if(isset($_POST['terminar2'])){
+	header('Location: http://localhost/dist/index.html');
+}
+
+?>
 </body>
 </html>
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,165 +1,163 @@
+$("#form").validate({
+    onkeyup: false,
+    onfocusout: false,
+    onclick: false,
+    rules: {
+        nombre: {
+            required: true
+        },
+        apellidos: {
+            required: true
 
- $("#form").validate({
-                onkeyup: false,
-                onfocusout: false,
-                onclick: false,
-                rules: {
-                     nombre: {
-                        required: true
-                    },
-                     apellidos: {
-                       required:true
-                        
-                    },
-                    tel: {
-                        required: true,
-                        maxlength:9,
-                        minlength:9,
-                        digits:true
+        },
+        tel: {
+            required: true,
+            maxlength: 9,
+            minlength: 9,
+            digits: true
 
-                    },
-                    email: {
-                       required:true,
-                       minlength:4,
-                       email:true,
-  					   remote:"php/validar_email_db.php"
-                    }, 
-                    remail: {
-                        required: true,
-                        minlength:4,
-                        equalTo: email
-                    },
-            		cp:{
-                      required:true,
-                      maxlength:5,
-                      minlength:5,
-                      digits:true 
-                    },
-                    iban:{
-                      /*ES91 2085 0166 69 0330150871*/
-                      iban:true,
-                      required:true
-                      
-                    },
-                    password: {
-                       required:true,  
-                       controlPass:true   
-                    }, 
-                    repassword: {
-                        required: true,
-                   		equalTo: password
-                    },
-                    cif: {
-                        required:true,
-                        cifES:true
-                    },
-                    nif: {
-                        required:true,
-                        nifES:true,
-                 		remote:"php/validar_nif_db.php"
-                    }
-                    
-                     
-                }
+        },
+        email: {
+            required: true,
+            minlength: 4,
+            email: true,
+            remote: "php/validar_email_db.php"
+        },
+        remail: {
+            required: true,
+            minlength: 4,
+            equalTo: email
+        },
+        cp: {
+            required: true,
+            maxlength: 5,
+            minlength: 5,
+            digits: true
+        },
+        iban: {
+            iban: true,
+        },
+        password: {
+            required: true,
+            controlPass: true
+        },
+        repassword: {
+            required: true,
+            equalTo: password
+        },
+        cif: {
+            required: true,
+            cifES: true
+        },
+        nif: {
+            required: true,
+            nifES: true,
+            remote: "php/validar_nif_db.php"
+        }
+    }
+});
 
 
-
-                });
-
-
-//si el cp tiene menos de 5 cifras relleno con ceros a la izq
-//cp entre 01000 y 52999
+//Rellenar el codigo postal si empieza por 0
 $("#cp").focusout(function() {
-                var caracteres = $("#cp").val();
-                var num=5-caracteres.length;
-                var cero="0";
+    var caracteres = $("#cp").val();
+    var num = 5 - caracteres.length;
+    var cero = "0";
+    if (num > 0) {
+        for (i = 1; i < num; i++) {
+            cero += "0";
+        }
+        $("#cp").val(cero + caracteres);
+    } else if (num <= 0) {
+        $("#cp").val(caracteres);
+    }
+    var cod = new Array(); 
+    cod[1] = "Alava";
+    cod[2] = "Albacete";
+    cod[3] = "Alicante";
+    cod[4] = "Almeria";
+    cod[5] = "Avila";
+    cod[6] = "Badajoz";
+    cod[7] = "Illes Balears";
+    cod[8] = "Barcelona";
+    cod[9] = "Burgos";
+    cod[10] = "Caceres";
+    cod[11] = "Cadiz";
+    cod[12] = "Castellon";
+    cod[13] = "Ciudad Real";
+    cod[14] = "Cordoba";
+    cod[15] = "A Coruña";
+    cod[16] = "Cuenca";
+    cod[17] = "Girona";
+    cod[18] = "Granada";
+    cod[19] = "Guadalajara";
+    cod[20] = "Guipuzcoa";
+    cod[21] = "Huelva";
+    cod[22] = "Huesca";
+    cod[23] = "Jaen";
+    cod[24] = "Leon";
+    cod[25] = "Lleida";
+    cod[26] = "La Rioja";
+    cod[27] = "Lugo";
+    cod[28] = "Madrid";
+    cod[29] = "Malaga";
+    cod[30] = "Murcia";
+    cod[31] = "Navarra";
+    cod[32] = "Ourense";
+    cod[33] = "Asturias";
+    cod[34] = "Palencia";
+    cod[35] = "Las Palmas";
+    cod[36] = "Pontevedra";
+    cod[37] = "Salamanca";
+    cod[38] = "S.C. Tenerife";
+    cod[39] = "Cantabria";
+    cod[40] = "Segovia";
+    cod[41] = "Sevilla";
+    cod[42] = "Soria";
+    cod[43] = "Tarragona";
+    cod[44] = "Teruel";
+    cod[45] = "Toledo";
+    cod[46] = "Valencia";
+    cod[47] = "Valladolid";
+    cod[48] = "Vizcaya";
+    cod[49] = "Zamora";
+    cod[50] = "Zaragoza";
+    cod[51] = "Ceuta";
+    cod[52] = "Melilla";
+    $cp = $("#cp").val();
+    $zip = $cp.substr(0, 2);
+    if ($zip == 00 || $cp < 1000 || $cp > 52999) {
+        alert("El zip no es correcto");
 
-                if(num>0){
-                for(i=1;i<num;i++){
-                  cero+="0";
-                }
+    }
+    //si los dos primeros digitos son menores de 10, me tengo que quedar solo con el 2ºnumero
+    if ($zip.substr(0, 1) == 0) {
+        $zip = $cp.substr(1, 1);
 
-                $("#cp").val(cero + caracteres);
-               }else if(num<=0){
-                $("#cp").val(caracteres);
-               }
-
-  var cod = new Array(); // Vector que asocia CP con Provincia
-  
-  
-  cod[1]="Alava";cod[2]="Albacete";cod[3]="Alicante";
-  cod[4]="Almeria";cod[5]="Avila";cod[6]="Badajoz";
-  cod[7]="Illes Balears";cod[8]="Barcelona";cod[9]="Burgos";
-  cod[10]="Caceres";
-  cod[11]="Cadiz";cod[12]="Castellon";cod[13]="Ciudad Real";
-  cod[14]="Cordoba";cod[15]="A Coruña";cod[16]="Cuenca";
-  cod[17]="Girona";cod[18]="Granada";cod[19]="Guadalajara";
-  cod[20]="Guipuzcoa";
-  cod[21]="Huelva";cod[22]="Huesca";cod[23]="Jaen";cod[24]="Leon";
-  cod[25]="Lleida";cod[26]="La Rioja";cod[27]="Lugo";cod[28]="Madrid";
-  cod[29]="Malaga";cod[30]="Murcia";
-  cod[31]="Navarra";cod[32]="Ourense";cod[33]="Asturias";cod[34]="Palencia";
-  cod[35]="Las Palmas";cod[36]="Pontevedra";cod[37]="Salamanca";
-  cod[38]="S.C. Tenerife";cod[39]="Cantabria";cod[40]="Segovia";
-  cod[41]="Sevilla";cod[42]="Soria";cod[43]="Tarragona";cod[44]="Teruel";
-  cod[45]="Toledo";cod[46]="Valencia";cod[47]="Valladolid";cod[48]="Vizcaya";
-  cod[49]="Zamora";cod[50]="Zaragoza";
-  cod[51]="Ceuta";cod[52]="Melilla"; 
-$cp=$("#cp").val();
- $zip = $cp.substr(0, 2);
- if($zip==00||$cp<1000||$cp>52999){
-   alert("el zip es erroneo");
-
- }
-
- //si los dos primeros digitos son menores de 10, me tengo que quedar solo con el 2ºnumero
- if($zip.substr(0,1)==0){
- 
-         $zip = $cp.substr(1,1);
-         
- }
- 
- $("#provincia").val(cod[$zip]);
-//$cpMunicipio=$("#cp").val();
- //alert($cpMunicipio);
-/*
- $("#cp").load("php/cp.php",{zip: $cpMunicipio});
-    */
+    }
+    $("#provincia").val(cod[$zip]);
     $.ajax({
-      url:"php/CodigoPostal.php",
-      type: "POST",
-      data:"zip="+$("#cp").val(),
-      success: function(opciones){
-        $("#localidad").html(opciones);
-      }
+        url: "php/CodigoPostal.php",
+        type: "POST",
+        data: "zip=" + $("#cp").val(),
+        success: function(opciones) {
+            $("#localidad").html(opciones);
+        }
     })
- 
-            });
-
-
-//despues de salir del foco de apellidos relleno el campo de facturacionNombre con el nmbre y apellidos
+});
+//Rellenar el campo nombre con el nombre y apellidos
 $("#apellidos").focusout(function() {
     var nombre = $("#nom").val();
     var apellidos = $("#apellidos").val();
     $("#nombreFac").val(nombre + " " + apellidos);
 
 });
-
-
-
-
-//el campo usuario se rellenara con el campo email
+//rellenar el campo usuario con el mail
 $("#remail").focusout(function() {
-
     $("#usuario").val($("#remail").val());
     $("#usuario").disabled = "true";
 });
-
-
-
-
-
-//cambiar etiquetas demandante
+//cambiar demandante
 $("#demandante").focusout(function() {
     if ($("#demandante").val() == "empre") {
         $("#etNIF").html("CIF:");
@@ -179,30 +177,22 @@ $("#demandante").focusout(function() {
     }
 });
 
-
+//Plugin complejidad contraseña
 $("#password").focusin(function() {
-
-                $("#password").complexify({}, function(valid, complexity){
-    //alert("Password complexity: " + complexity);
-
-          $("#pass").val(complexity);
-
-    
-  });
+    $("#password").complexify({}, function(valid, complexity) {
+        $("#pass").val(complexity);
+    });
 
 
 });
+
 //complejidad de la contraseña, minimo 30% para validar
-jQuery.validator.addMethod("controlPass", function(value,element) {
+jQuery.validator.addMethod("controlPass", function(value, element) {
+    var prueba = $("#pass").val();
+    if (prueba < 30) {
+        return false;
+    } else {
 
-  var prueba=$("#pass").val();
-
-  if(prueba<30){
-   // alert("ERROR");
-    return false;
-  }else{
-     
-
-  return true;
-  }
-}, "Complejidad demasiado baja");
+        return true;
+    }
+}, "Complejidad baja");
